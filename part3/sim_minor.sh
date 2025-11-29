1df0512618d0d5b2b6eb6402f4e32d93f3ebec89
andrew-carter1 <andrew.carter1@ufl.edu>
2025-11-29 00:00:04 -0500

    more flexible script

--- part3/sim_minor.sh
+++ part3/sim_minor.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
 
-${GEM5_PATH}/build/X86/gem5.opt $@ ./config_minor.py
+if [ -f "${GEM5_PATH}/build/ALL/gem5.opt" ]; then
+    GEM5_BIN="${GEM5_PATH}/build/ALL/gem5.opt"
+elif [ -f "${GEM5_PATH}/build/X86/gem5.opt" ]; then
+    GEM5_BIN="${GEM5_PATH}/build/X86/gem5.opt"
+else
+    echo "Error: Could not find gem5.opt in build/ALL or build/X86"
+    exit 1
+fi
+
+${GEM5_BIN} $@ ./config_minor.py
